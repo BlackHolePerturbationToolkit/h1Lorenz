6c0f2026978d98a938bc1c6fa47007e1577032fe
NielsW <niels.warburton@ucd.ie>
2023-06-26 22:53:33 +0100

    Remove notebooks and grid files from repository

--- notebooks/ComputeLorenzDataForManyRadii.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-r0i = 9
-while r0i <= 30:
-	r0f = 9
-	while r0f <= 9:
-		subprocess.run(["math", "-script", "Circular_Lorenz_output_horiz_and_inf_fields_script.m", str(r0i), str(r0f)])
-		r0f += 1
-	r0i += 1
